0c936e6d8d16b4734b72fe6f0857493f37dc29c0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 16:48:35 +0200

    create users table

---
+++ schemas.sql
@@ -0,0 +1,9 @@
+-- Create table 1 (users)
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    profile_pic_url VARCHAR(256)
+);
