9db53abcdc34e91a7f3947e77d35e2a4aa0b51fc
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 16:57:19 +0200

    add data to houses table

---
+++ house_queries.sql
@@ -0,0 +1,7 @@
+-- House Queries
+-- Create House
+INSERT INTO
+        houses (location, bedrooms, bathroom, price, description)
+VALUES
+        ('Phuket, Thailand', 1, 1, 100, 'Beautiful villa with private pool')
+;
