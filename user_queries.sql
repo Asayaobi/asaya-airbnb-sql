80a3a560ee33c52b7726aa005cac0a4907a3a554
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 16:49:41 +0200

    Add data to users table

---
+++ user_queries.sql
@@ -0,0 +1,6 @@
+-- User Queries
+-- Register (Create)
+INSERT INTO users (first_name, last_name, email, password, profile_pic_url)
+    VALUES
+        ('Jean', 'Dupont', 'jdupont@paris.com', 'francais', 'https://randomuser.me/api/portraits/men/89.jpg')
+;
