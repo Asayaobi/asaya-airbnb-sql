bffbb84f98b232af48832de4dfde0fabcc04e359
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 16:50:43 +0200

    add login query for users

--- user_queries.sql
+++ user_queries.sql
@@ -4,3 +4,8 @@ INSERT INTO users (first_name, last_name, email, password, profile_pic_url)
     VALUES
         ('Jean', 'Dupont', 'jdupont@paris.com', 'francais', 'https://randomuser.me/api/portraits/men/89.jpg')
 ;
+-- Login (Read)
+SELECT email, password
+FROM users
+    WHERE (email = 'jdupont@paris.com' AND password = 'francais')
+;
