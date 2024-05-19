37916564466fdb26b39d5894c7f5bdf981aa95e8
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 17:32:56 +0200

    Add query to read booking

--- bookings _query.sql
+++ bookings _query.sql
@@ -4,3 +4,17 @@ INSERT INTO bookings(user_id, house_id, booking_start_date, booking_end_date, pr
   VALUES
     (1, 4, '2024-03-10', '2024-03-12', 800)
 ;
+
+-- Read Booking
+SELECT 
+  bookings.house_id, houses.location, houses.bedrooms, houses.bathrooms, bookings.price, reviews.star_rating 
+FROM 
+  bookings 
+FULL JOIN houses
+ON bookings.house_id = houses.house_id
+FULL JOIN reviews
+ON bookings.house_id = reviews.house_id
+FULL JOIN pictures
+ON bookings.house_id = pictures.house_id
+WHERE user_id = 1
+;
