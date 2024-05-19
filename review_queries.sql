458c6c61e8c92d6e87126002f3a1d34ed9ea61f6
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 17:39:36 +0200

    Add query to insert data to reviews

---
+++ review_queries.sql
@@ -0,0 +1,6 @@
+-- Review Queries
+-- Create Review
+INSERT INTO reviews (reviewer_id, house_id, review_text, star_rating, review_date)
+    VALUES
+        (2, 1, 'Too much gold everywhere. Incredibly tacky.', 1, 2024-02-01)
+;
