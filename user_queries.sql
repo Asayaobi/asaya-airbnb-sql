9b45d3999078de4f03fda1e8f7481eb95dd9c4bf
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 16:51:52 +0200

    Add query to update user

--- user_queries.sql
+++ user_queries.sql
@@ -4,8 +4,15 @@ INSERT INTO users (first_name, last_name, email, password, profile_pic_url)
     VALUES
         ('Jean', 'Dupont', 'jdupont@paris.com', 'francais', 'https://randomuser.me/api/portraits/men/89.jpg')
 ;
+
 -- Login (Read)
 SELECT email, password
 FROM users
     WHERE (email = 'jdupont@paris.com' AND password = 'francais')
+;
+
+-- Update Profile
+UPDATE users
+    SET profile_pic_url = 'https://randomuser.me/api/portraits/men/89.jpg', first_name = 'Jonny'
+    WHERE user_id = 1
 ;
